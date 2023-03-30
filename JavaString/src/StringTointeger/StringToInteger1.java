8a38a9fdbbfcfd03b8c080f727b0affc574a2071
dell <dell@DESKTOP-CKOQTON>
2023-03-30 22:19:25 +0530

    java test

---
+++ JavaString/src/StringTointeger/StringToInteger1.java
@@ -0,0 +1,11 @@
+package StringTointeger;
+
+public class StringToInteger1 {
+
+	public static void main(String[] args) {
+		String s = "100";
+		int i = Integer.parseInt(s);
+        System.out.println(i);
+	}
+
+}
